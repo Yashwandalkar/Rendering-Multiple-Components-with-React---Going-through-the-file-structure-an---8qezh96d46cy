9e396d36b6781d9b0ce2b4ae1fb39499a1880eef
Yash Wandalkar <yash.wandalkar@gmail.com>
2022-09-19 07:59:31 +0000

    Update App.js and index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <>
+      <div id="main"></div>
+      <h1>Amazone Clone</h1>
+      <p>
+        {" "}
+        It's an eCommerce app inspired by Amazon, big Basket, Grofers, grocery
+        app, a sample Android application 📱 built to demonstrate the use of
+        Modern Android development tools - (Kotlin, Coroutines, Flow, Dagger
+        2/Hilt, Architecture Components, MVVM, Room, Retrofit, Gson, Material
+        Components).
+      </p>
+    </>
+  );
+};
 
 export default App;
